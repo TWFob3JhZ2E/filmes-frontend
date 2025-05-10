7fbefc2c12d71c01095c4520bfc6efadac9fefcb
TWFob3JhZ2E <ScottPetrov280@proton.me>
2025-05-09 23:35:47 -0300

    servidor - segurraca 3.1

--- FrontEnd/Conex/Config.js
+++ FrontEnd/Conex/Config.js
@@ -1,3 +1,2 @@
 const BACKEND_URL = "https://filmes-flask.onrender.com";
-const API_KEY = "aW9!7sd9e8e98jzK$p3Rt6yU*IUHhusdhibH2nCvE8q"; // Se quiser deixar aqui centralizado
-
+const API_KEY = "aW9!7sd9e8e98jzK$p3Rt6yU*IUHhusdhibH2nCvE8q"; // Chave API
