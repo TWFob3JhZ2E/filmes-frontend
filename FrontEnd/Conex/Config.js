844722778e9248d3ac50706a5444ab86c3eedf39
TWFob3JhZ2E <ScottPetrov280@proton.me>
2025-05-19 18:06:54 -0300

    front para o back

--- FrontEnd/Conex/Config.js
+++ FrontEnd/Conex/Config.js
@@ -1,3 +1,3 @@
-const BACKEND_URL = "https://www.mystarmovies.online/";
+const BACKEND_URL = "https://filmes-flask.onrender.com";
 const CSRF_TOKEN_ENDPOINT = `${BACKEND_URL}/get-csrf-token`;
 const API_KEY = "x9k3m7p2q8w4z6t1";
